0905143f78ed1a43736da9ad8939ed25d0323921
TheNombril <fadiel57@hotmail.fr>
2017-03-21 11:54:34 +0100

    Delete Github.bat

--- Site Julien_Maxime/Github.bat
+++
@@ -1,6 +0,0 @@
-echo "# Roulette_Masters" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/TheNombril/Roulette_Masters.git
-git push -u origin master
